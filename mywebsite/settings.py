2a156533f1e9dbeb987fa3dbad75440b9ba07b2c
smk37git <sdmain3@gmail.com>
2025-03-07 12:35:16 -0600

    Changed Profile Picture directory to match 'ACCOUNTS'

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Media files (User uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join('ACCOUNTS/static/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
