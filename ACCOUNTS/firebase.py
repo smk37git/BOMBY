cf1a1e54d845d1c613c97dbad31a21dcdf6acb8c
smk37git <sdmain3@gmail.com>
2025-03-08 17:42:48 -0600

    Turned Firebase off

--- ACCOUNTS/firebase.py
+++ ACCOUNTS/firebase.py
@@ -9,7 +9,7 @@ import sys
 logger = logging.getLogger(__name__)
 
 # Feature flag - set to False to completely disable Firebase
-USE_FIREBASE = True
+USE_FIREBASE = False
 
 # Initialize variables
 firebase_app = None
