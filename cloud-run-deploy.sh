fdb3d7c7ca4b370e4ca18b959e2c4fbbb1408371
smk37git <sdmain3@gmail.com>
2025-03-09 18:46:30 -0500

    Google Bucket Update for PFPs

--- cloud-run-deploy.sh
+++ cloud-run-deploy.sh
@@ -14,6 +14,7 @@ DB_USER="postgres"
 DB_PASSWORD=$(grep DB_PASSWORD .env | cut -d'=' -f2)
 SENDGRID_KEY=$(grep SENDGRID_API_KEY .env | cut -d'=' -f2)
 DEFAULT_FROM_EMAIL=$(grep DEFAULT_FROM_EMAIL .env | cut -d'=' -f2)
+GS_BUCKET_NAME="bomby-database"
 
 # Build the Docker image
 echo "Building Docker image..."
@@ -43,8 +44,21 @@ gcloud run deploy $SERVICE_NAME \
   --platform managed \
   --region $REGION \
   --allow-unauthenticated \
-  --set-env-vars="DEBUG=False,ALLOWED_HOSTS=.run.app,$SERVICE_NAME.run.app,SENDGRID_API_KEY=$SENDGRID_KEY,DEFAULT_FROM_EMAIL=$DEFAULT_FROM_EMAIL,SENDGRID_SANDBOX_MODE=False,DB_NAME=$DB_NAME,DB_USER=$DB_USER,DB_HOST=/cloudsql/$INSTANCE_CONNECTION_NAME,GS_BUCKET_NAME=bomby-database" \
-  --set-secrets="DJANGO_SECRET_KEY=django-secret-key:latest,DB_PASSWORD=postgres-password:latest,AWS_ACCESS_KEY_ID=aws-access-key:latest,AWS_SECRET_ACCESS_KEY=aws-secret-key:latest" \
+  --set-env-vars="DEBUG=False,\
+ALLOWED_HOSTS=.run.app,$SERVICE_NAME.run.app,\
+SENDGRID_API_KEY=$SENDGRID_KEY,\
+DEFAULT_FROM_EMAIL=$DEFAULT_FROM_EMAIL,\
+SENDGRID_SANDBOX_MODE=False,\
+DB_NAME=$DB_NAME,\
+DB_USER=$DB_USER,\
+DB_HOST=/cloudsql/$INSTANCE_CONNECTION_NAME,\
+GS_BUCKET_NAME=$GS_BUCKET_NAME,\
+GS_PROJECT_ID=$PROJECT_ID,\
+USE_GCS=True" \
+  --set-secrets="DJANGO_SECRET_KEY=django-secret-key:latest,\
+DB_PASSWORD=postgres-password:latest,\
+AWS_ACCESS_KEY_ID=aws-access-key:latest,\
+AWS_SECRET_ACCESS_KEY=aws-secret-key:latest" \
   --memory 512Mi \
   --add-cloudsql-instances=$INSTANCE_CONNECTION_NAME
 
