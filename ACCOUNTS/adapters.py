87164cf28706794be6cae3095b467882274ff0c0
smk37git <sdmain3@gmail.com>
2025-03-12 13:40:29 -0500

    Updated adapter

--- ACCOUNTS/adapters.py
+++ ACCOUNTS/adapters.py
@@ -5,32 +5,23 @@ from django.shortcuts import redirect
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
-        """
-        Invoked just after a user successfully authenticates via a social provider,
-        but before the login is actually processed.
-        """
-        # Get the user from the sociallogin object
-        user = sociallogin.user
-
-        # Check if user already exists in the system
-        if user.id:
-            # User exists, just proceed with login
+        # Auto-connect if email exists
+        if sociallogin.is_existing:
             return
             
-        # This is a new user, set default values
+        # Extract email from social account
+        email = sociallogin.account.extra_data.get('email')
+        if email:
+            # Try to find existing user with this email
+            try:
+                user = User.objects.get(email=email)
+                # Connect social account to existing user
+                sociallogin.connect(request, user)
+            except User.DoesNotExist:
+                pass
+
+    def save_user(self, request, sociallogin, form=None):
+        user = super().save_user(request, sociallogin, form)
         user.user_type = user.UserType.MEMBER
-        
-    def populate_user(self, request, sociallogin, data):
-        """Customize user creation from social account data"""
-        user = super().populate_user(request, sociallogin, data)
-        user.user_type = user.UserType.MEMBER
+        user.save()
         return user
-        
-    def get_connect_redirect_url(self, request, socialaccount):
-        """Override to redirect to your custom URL after connecting"""
-        return settings.LOGIN_REDIRECT_URL
-        
-class CustomAccountAdapter(DefaultAccountAdapter):
-    def get_login_redirect_url(self, request):
-        """Where to redirect after successful login"""
-        return settings.LOGIN_REDIRECT_URL
