5d8a7a70fb29c713a9de0f83148e948ccd0b0e6f
smk37git <sdmain3@gmail.com>
2025-04-11 13:16:46 -0500

    Updated Allowed Hostss and CSRF trusted Origins

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -38,7 +38,8 @@ DEBUG = True
 #DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else []
-CSRF_TRUSTED_ORIGINS = ['https://bomby-799218251279.us-central1.run.app']
+ALLOWED_HOSTS += ['bomby.us', 'www.bomby.us', 'bomby-799218251279.us-central1.run.app']
+CSRF_TRUSTED_ORIGINS = ['https://bomby.us', 'http://bomby.us', 'https://bomby-799218251279.us-central1.run.app']
 
 
 # Application definition
