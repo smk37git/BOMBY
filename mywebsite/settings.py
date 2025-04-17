ee3e235382861ba99c0e08f12686c2fa21a189be
smk37git <sdmain3@gmail.com>
2025-04-17 16:26:52 -0500

    Updated manifest and changed whitenoise static storage

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -148,7 +148,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 WHITENOISE_MIMETYPES = {
     '.css': 'text/css',
