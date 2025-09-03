859d7bcceca3c0a23dc12af7a1db17c8f2a03343
smk37git <sdmain3@gmail.com>
2025-09-02 20:25:31 -0500

    Removed local database if debug

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -157,14 +157,6 @@ DATABASES = {
     }
 }
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
