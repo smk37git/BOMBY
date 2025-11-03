e1eee3606a613820441f5cf147fc92e7a98b69f5
smk37git <sdmain3@gmail.com>
2025-11-02 23:42:43 -0600

    Updated settings with updates tab

--- FUZEOBS/views.py
+++ FUZEOBS/views.py
@@ -26,9 +26,9 @@ User = get_user_model()
 @require_http_methods(["GET"])
 def fuzeobs_check_update(request):
     return JsonResponse({
-        'version': '0.9.1',
+        'version': '0.9.2',
         'download_url': 'https://storage.googleapis.com/fuzeobs-public/fuzeobs-installer/FuzeOBS-Installer.exe',
-        'changelog': 'Updated version number graphics',
+        'changelog': 'Updated settings with updates tab',
         'mandatory': False
     })
 
