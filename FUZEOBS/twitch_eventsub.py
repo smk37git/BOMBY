eb2aed5f3b38c84418d9b7059301d0a7c7bba101
smk37git <sdmain3@gmail.com>
2025-11-17 23:08:54 -0600

    Changed to take twitch secret not OAuth token

--- FUZEOBS/twitch_eventsub.py
+++ FUZEOBS/twitch_eventsub.py
@@ -3,10 +3,21 @@ from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 from django.conf import settings
 
-def subscribe_twitch_events(user_id, broadcaster_id, access_token):
+def get_app_access_token():
+    """Get app access token for EventSub"""
+    resp = requests.post('https://id.twitch.tv/oauth2/token', data={
+        'client_id': settings.TWITCH_CLIENT_ID,
+        'client_secret': settings.TWITCH_CLIENT_SECRET,
+        'grant_type': 'client_credentials'
+    })
+    return resp.json()['access_token']
+
+def subscribe_twitch_events(user_id, broadcaster_id, user_access_token):
     """Subscribe to Twitch events"""
+    app_token = get_app_access_token()
+    
     headers = {
-        'Authorization': f'Bearer {access_token}',
+        'Authorization': f'Bearer {app_token}',
         'Client-Id': settings.TWITCH_CLIENT_ID,
         'Content-Type': 'application/json'
     }
