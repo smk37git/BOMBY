9fbeb27d6e2d392e564418709e1ad119042d301f
smk37git <sdmain3@gmail.com>
2025-11-30 21:54:50 -0600

    v0.9.11 Updater Improvements

--- FUZEOBS/views.py
+++ FUZEOBS/views.py
@@ -84,9 +84,9 @@ def cleanup_old_sessions():
 @require_http_methods(["GET"])
 def fuzeobs_check_update(request):
     return JsonResponse({
-        'version': '0.9.10',
+        'version': '0.9.11',
         'download_url': 'https://storage.googleapis.com/fuzeobs-public/fuzeobs-installer/FuzeOBS-Installer.exe',
-        'changelog': 'Massive Tools-Tab update; multiple widgets added; platform connections added',
+        'changelog': 'Updater improvements',
         'mandatory': False
     })
 
