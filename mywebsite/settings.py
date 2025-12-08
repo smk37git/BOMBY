bb2ef0d9d0a0106ec8725c8fbb7e568f1b54abf5
smk37git <sdmain3@gmail.com>
2025-12-08 09:50:23 -0600

    Updated tiktok client secret

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False #
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else []
 ALLOWED_HOSTS = ['bomby.us', 'www.bomby.us', 'bomby-799218251279.us-central1.run.app', 'localhost', '127.0.0.1']
@@ -371,5 +371,5 @@ FACEBOOK_CLIENT_ID = os.environ.get('FACEBOOK_CLIENT_ID')
 FACEBOOK_CLIENT_SECRET = os.environ.get('FACEBOOK_CLIENT_SECRET')
 
 # TikTok Keys
-TIKTOK_CLIENT_ID = os.environ.get('TIKTOK_CLIENT_ID')
+TIKTOK_CLIENT_KEY = os.environ.get('TIKTOK_CLIENT_KEY')
 TIKTOK_CLIENT_SECRET = os.environ.get('TIKTOK_CLIENT_SECRET')
