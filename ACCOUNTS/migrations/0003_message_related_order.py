84de497dfa31c0ef218586f4faa8c29155c5b610
smk37git <sdmain3@gmail.com>
2025-12-11 18:33:38 -0600

    ACCOUNTS fixed faulty migration

--- ACCOUNTS/migrations/0003_message_related_order.py
+++ ACCOUNTS/migrations/0003_message_related_order.py
@@ -1,6 +1,27 @@
 from django.db import migrations, models
 import django.db.models.deletion
 
+
+def add_related_order_if_not_exists(apps, schema_editor):
+    """Only add column if it doesn't exist"""
+    connection = schema_editor.connection
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT column_name FROM information_schema.columns 
+            WHERE table_name = 'ACCOUNTS_message' AND column_name = 'related_order_id'
+        """)
+        if not cursor.fetchone():
+            cursor.execute("""
+                ALTER TABLE "ACCOUNTS_message" 
+                ADD COLUMN "related_order_id" bigint NULL 
+                REFERENCES "STORE_order" ("id") DEFERRABLE INITIALLY DEFERRED
+            """)
+            cursor.execute("""
+                CREATE INDEX "ACCOUNTS_message_related_order_id_idx" 
+                ON "ACCOUNTS_message" ("related_order_id")
+            """)
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ('ACCOUNTS', '0002_remove_user_fuzeobs_active_and_more'),
@@ -8,9 +29,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='message',
-            name='related_order',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='STORE.order'),
-        ),
+        migrations.RunPython(add_related_order_if_not_exists, migrations.RunPython.noop),
     ]
